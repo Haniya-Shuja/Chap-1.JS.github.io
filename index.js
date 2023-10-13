41d125cfcce48ad659fe97b3813da8ee8bf24e79
Haniya-Shuja <116838089+Haniya-Shuja@users.noreply.github.com>
2023-10-13 19:20:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+// Practice exercise 1.1
+
+console.log ("Hello...")
+
+console.log(4+10);
+
+alert("Hi there")
+
+console.log(prompt("what is your name"));
+
+// console.log(math.random()100);
+// Practice exercise 1.2
+
+console.log("Hello world")
+
+
+// Practice exercise 1.3
+
+let status = "new";
+let scared = true;
+if (status === "new") {
+console.log("Welcome to JavaScript!");
+if (scared) {
+console.log("Don't worry you will be fine!");
+} else {
+console.log("You're brave! You are going to do great!");
+}
+} else {
+console.log("Welcome back, I knew you'd like it!");
+}
+
+// Practice exercise 1.4
+
+
+// Step 1: Add a new statement to set a variable value
+let a = 10; // Setting a value of 10
+
+// Step 2: Print the value using console.log()
+console.log(a); // This will print the value of variable 'a' to the console
+
+// Step 3: At the end of your JavaScript code, use a multiple-line comment
+/*
+   This is a multiple-line comment.
+   In a real production script, you might use this space
+   to add a brief outline of the purpose of the file.
+*/
+prompt("Hi! How are you?");
+
+
+
+
+console.log(Math.random() * 100);
+// // SELF-CHECK QUIZ
+// /*
+// This is a multiple-line comment.
+// You can write as many lines of comments as needed within these delimiters.
+// Comments are ignored by the JavaScript interpreter.
+// */
+
+
+// /*
+// This function calculates the sum of two numbers.
+
+// Parameters:
+// - num1: The first number.
+// - num2: The second number.
+
+// Returns:
+// The sum of num1 and num2.
+// */
+// function addNumbers(num1, num2) {
+//     // This is a single-line comment inside the function
+//     return num1 + num2;
+// }
+
+// /*
+// You can also use multiple-line comments to comment out a block of code:
+
+// function unusedFunction() {
+//     // This code is commented out and will not run
+//     // It can be useful for debugging or temporarily disabling code
+// }
+// */
+
+// // Call the function
+// const result = addNumbers(5, 3);
+// console.log(result); // Output: 8
+
+
+
+
